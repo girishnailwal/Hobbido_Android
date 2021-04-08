97a2c13c55b66cc498b5130cc88a038a5e4c31b7
sanjeevkumar11141@gmail.com <sanjeevkumar11141@gmail.com>
2021-04-08 18:58:20 +0530

    ferman khan 08.04.2021

---
+++ app/src/main/java/com/hobbido/app/deleteAccount/DeleteAccountActivity.java
@@ -0,0 +1,16 @@
+package com.hobbido.app.deleteAccount;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hobbido.app.R;
+
+public class DeleteAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_account);
+    }
+}
