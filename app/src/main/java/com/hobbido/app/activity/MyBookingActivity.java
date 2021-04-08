226b28cf059300d8f30c09269aa6409cbebd77c9
sanjeev.kumar@mobulous.com <sanjeev.kumar@mobulous.com>
2021-04-08 11:23:21 +0530

    08.04.2021

--- app/src/main/java/com/hobbido/app/activity/MyBookingActivity.java
+++ app/src/main/java/com/hobbido/app/activity/MyBookingActivity.java
@@ -1,16 +1,20 @@
 package com.hobbido.app.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.hobbido.app.R;
+import com.hobbido.app.fragment.HistoryFragment;
 import com.hobbido.app.fragment.PhoneNumberFragment;
 import com.hobbido.app.fragment.RegularFragment;
+import com.hobbido.app.fragment.UpComingFragment;
 import com.hobbido.app.fragment.WaitingListFragment;
 
 public class MyBookingActivity extends AppCompatActivity implements View.OnClickListener {
@@ -39,18 +43,75 @@ public class MyBookingActivity extends AppCompatActivity implements View.OnClick
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.watingList_btn:
+                selectedWaitListingButton();
                 showWaitingListFragment();
                 break;
 
             case R.id.upcoming_btn:
-                showWaitingListFragment();
+                selectedUpComingButton();
+                showUpComingFragment();
                 break;
 
             case R.id.history_btn:
-                showWaitingListFragment();
+                selectedHistoryButton();
+                showHistoryFragment();
                 break;
 
         }
+    }
+
+
+    public void selectedWaitListingButton(){
+        watingList_btn.setTextColor(getResources().getColor(R.color.white));
+        upcoming_btn.setTextColor(getResources().getColor(R.color.ColorLightBlack));
+        history_btn.setTextColor(getResources().getColor(R.color.ColorLightBlack));
+
+        watingList_btn.setBackground(getResources().getDrawable(R.drawable.rounded_purpale_bg));
+        upcoming_btn.setBackground(getResources().getDrawable(R.drawable.unselected_gray_btn_bg));
+        history_btn.setBackground(getResources().getDrawable(R.drawable.unselected_gray_btn_bg));
+
+        Typeface typeface1 = ResourcesCompat.getFont(this, R.font.montserrat_semibold);
+        watingList_btn.setTypeface(typeface1);
+
+        Typeface typeface2 = ResourcesCompat.getFont(this, R.font.montserrat_regular);
+        upcoming_btn.setTypeface(typeface2);
+        history_btn.setTypeface(typeface2);
+    }
+
+
+    public void selectedUpComingButton(){
+        upcoming_btn.setTextColor(getResources().getColor(R.color.white));
+        watingList_btn.setTextColor(getResources().getColor(R.color.ColorLightBlack));
+        history_btn.setTextColor(getResources().getColor(R.color.ColorLightBlack));
+
+        upcoming_btn.setBackground(getResources().getDrawable(R.drawable.rounded_purpale_bg));
+        watingList_btn.setBackground(getResources().getDrawable(R.drawable.unselected_gray_btn_bg));
+        history_btn.setBackground(getResources().getDrawable(R.drawable.unselected_gray_btn_bg));
+
+        Typeface typeface1 = ResourcesCompat.getFont(this, R.font.montserrat_semibold);
+        upcoming_btn.setTypeface(typeface1);
+
+        Typeface typeface2 = ResourcesCompat.getFont(this, R.font.montserrat_regular);
+        watingList_btn.setTypeface(typeface2);
+        history_btn.setTypeface(typeface2);
+    }
+
+
+    public void selectedHistoryButton(){
+        history_btn.setTextColor(getResources().getColor(R.color.white));
+        watingList_btn.setTextColor(getResources().getColor(R.color.ColorLightBlack));
+        upcoming_btn.setTextColor(getResources().getColor(R.color.ColorLightBlack));
+
+        history_btn.setBackground(getResources().getDrawable(R.drawable.rounded_purpale_bg));
+        watingList_btn.setBackground(getResources().getDrawable(R.drawable.unselected_gray_btn_bg));
+        upcoming_btn.setBackground(getResources().getDrawable(R.drawable.unselected_gray_btn_bg));
+
+        Typeface typeface1 = ResourcesCompat.getFont(this, R.font.montserrat_semibold);
+        history_btn.setTypeface(typeface1);
+
+        Typeface typeface2 = ResourcesCompat.getFont(this, R.font.montserrat_regular);
+        watingList_btn.setTypeface(typeface2);
+        upcoming_btn.setTypeface(typeface2);
     }
 
 
@@ -63,5 +124,21 @@ public class MyBookingActivity extends AppCompatActivity implements View.OnClick
         fragmentTransaction.commit();
     }
 
+    public void showUpComingFragment() {
+        UpComingFragment fragment = new UpComingFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left, R.anim.enter_from_left, R.anim.exit_to_right);
+        fragmentTransaction.replace(R.id.fragment_container, fragment, PhoneNumberFragment.TAG);
+        fragmentTransaction.commit();
+    }
 
+    public void showHistoryFragment() {
+        HistoryFragment fragment = new HistoryFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left, R.anim.enter_from_left, R.anim.exit_to_right);
+        fragmentTransaction.replace(R.id.fragment_container, fragment, PhoneNumberFragment.TAG);
+        fragmentTransaction.commit();
+    }
 }
